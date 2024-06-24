90f44dfe3df5747c62137ec5a1b96a2f78ceb9bd
ramesh-kunwar <rameshkunwar1110@gmail.com>
2024-06-24 21:52:44 +0545

    header and main nav

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const MainNav = () => {
-  return <div>MainNav</div>;
+  return (
+    <div>
+      <button className="px-4 py-2 font-bold hover:text-orange-500 hover:bg-white">
+        Login
+      </button>
+    </div>
+  );
 };
 
 export default MainNav;
